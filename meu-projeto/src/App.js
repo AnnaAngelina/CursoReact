41e5f17d405747431a6f7528dbf20a8585383f3c
Anna Angelina <annaangeh169@gmail.com>
2025-03-16 15:41:27 -0300

    entendendo JSX
    interpolação de variáveis, funções e algumas observações

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 
 function App() {
+  const nome = "Anna Angelina"
+  const nome_maiusculo = nome.toUpperCase()
+
+  function soma(a, b) {
+    return a + b
+  }
   return (
     <div className="App">
       <h1>Olá, React</h1>
-      <p>Minha primeira aplicação</p>
+      <p>Primeira aplicação de {nome} {nome_maiusculo}</p>
+      <p>Soma {soma(1, 3)}</p>
     </div>
   );
 }
